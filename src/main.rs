382e99f49f9cead061d6062c4e08a5c922215a5d
Ishi Inanis <ishi.inanis@gmail.com>
2022-02-21 17:52:36 +0300

    dice calc

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
-use std::io;
+use std::{io, thread};
+use std::time::Duration;
 use regex::Regex;
 
 /*
@@ -14,8 +15,7 @@ fn main() {
     println!("'Dice' also must be greater 0");
     println!("So far can only add values");
     println!("=======================");
-
-    println!("Enter formula without whitespaces and parentheses: ^(\\d+k\\d+[-+]*)+$");
+    println!("Enter formula without whitespaces and parentheses: (Count)k(Dice)(+-)...");
 
     let mut input = String::new();
 
@@ -23,40 +23,43 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    let cond = Regex::new(r"^(\d+k\d+[-+]*)+$").unwrap().is_match(input.trim());
+    let cond = Regex::new(r"^(\d+k\d+[-+]?)+$").unwrap().is_match(input.trim());
 
     if cond {
         let formula = input.trim();
-        let collect: Vec<&str> = input.trim().split(&['+']).collect(); // TODO: add handling minus '-'
 
-        let mut value: u16 = 0;
+        let mut value: i16 = 0;
 
-        println!("input: {}", input);
+        if formula.find(['+', '-']).is_some() {
+            let mut start: usize = 0;
 
-        for string in collect {
-            if !string.is_empty() {
-                let collect: Vec<&str> = string.split('k').collect();
+            while start < formula.len() {
+                let index = match formula[start..].find(['+', '-']) {
+                    None => formula.len(),
+                    Some(num) => start + num
+                };
+                let current = calc_k(&formula[start..index]);
 
-                let count: u16 = collect[0]
-                    .trim()
-                    .parse()
-                    .expect("This is not a number");
+                let mut operator = "+";
 
-                let dice: u16 = collect[1]
-                    .trim()
-                    .parse()
-                    .expect("This is not a number");
-
-                if count > 0 && dice > 0 {
-                    for _ in 1..=count {
-                        let current = rand::thread_rng().gen_range(0..=dice);
-
-                        value += current;
-
-                        println!("1k{} {}", dice, current);
-                    }
+                if start != 0 {
+                    operator = &formula[start - 1..start];
                 }
+
+                if operator == "+" {
+                    value += current;
+                } else {
+                    value -= current;
+                }
+
+                start = index + 1;
             }
+        } else {
+            value = calc_k(formula);
+        }
+
+        if value < 0 {
+            value = 0;
         }
 
         println!("{} = {}", formula, value);
@@ -64,5 +67,35 @@ fn main() {
         println!("This is not a formula");
         println!("Please see the instructions");
     }
+
     println!("=======================");
+
+    thread::sleep(Duration::from_secs(5));
+}
+
+fn calc_k(string: &str) -> i16 {
+    let mut value = 0;
+    let collect: Vec<&str> = string.split('k').collect();
+
+    let count: i16 = collect[0]
+        .trim()
+        .parse()
+        .expect("This is not a number");
+
+    let dice: i16 = collect[1]
+        .trim()
+        .parse()
+        .expect("This is not a number");
+
+    if count > 0 && dice > 0 {
+        for _ in 1..=count {
+            let current = rand::thread_rng().gen_range(0..=dice);
+
+            value += current;
+
+            println!("1k{} {}", dice, current);
+        }
+    }
+
+    value
 }
